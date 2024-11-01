38c477db70f73489160c69417d1f93d93db79fb5
HS-0914 <gudtn2238@gmail.com>
2024-11-01 16:23:44 +0900

    cors, csrf, xss 설정

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,14 @@ import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: true,
+    credentials: true,
+    exposedHeaders: ['Authorization'],
+  });
   app.use(cookieParser());
   await app.listen(8000);
 }
 bootstrap();
 
-// https://blog.naver.com/pjt3591oo/222991359835
-// https://velog.io/@dragonsu/Kafka-Kafka-producer-in-Nestjs-2-
-// https://velog.io/@atesi/nestjs-마이크로서비스-Kafka-구성하기
+// https://velog.io/@kwontae1313/NestJS-CORS - nestjs cors 설정

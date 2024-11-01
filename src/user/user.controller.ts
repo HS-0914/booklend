3484d0426155d1cec481d7f422e0e1fe7a7c47e3
HS-0914 <gudtn2238@gmail.com>
2024-11-01 16:14:59 +0900

    cors, csrf, xss 설정

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -8,60 +8,64 @@ import { JwtService } from '@nestjs/jwt';
 
 @Controller('user')
 export class UserController {
-    constructor(private userService: UserService, private jwtService: JwtService) {}
+  constructor(
+    private userService: UserService,
+    private jwtService: JwtService,
+  ) {}
 
-    // 회원 가입
-    @Post('/register')
-    @UsePipes(ValidationPipe)
-    async registerAccount(@Body() userDTO: UserDTO): Promise<UserDTO> {
-        return await this.userService.registerUser(userDTO);
-    }
+  // 회원 가입
+  @Post('/register')
+  @UsePipes(ValidationPipe)
+  async registerAccount(@Body() userDTO: UserDTO): Promise<UserDTO> {
+    return await this.userService.registerUser(userDTO);
+  }
 
-    // 로그인
-    @Post('/login')
-    @UsePipes(ValidationPipe)
-    async login(@Body() userDTO: UserDTO, @Res() res: Response): Promise<any> {
-        const jwt: Payload = await this.userService.validateUser(userDTO);
-        const accessToken = this.jwtService.sign(jwt)
-        res.setHeader('Authorization', 'Bearer ' + accessToken);
-        res.cookie('AuthToken', accessToken, {
-            httpOnly: true,
-            maxAge: 3 * 60 * 1000, // 1000 ms
-        })
-        return res.json({ accessToken: accessToken });
-    }
+  // 로그인
+  @Post('/login')
+  @UsePipes(ValidationPipe)
+  async login(@Body() userDTO: UserDTO, @Res() res: Response): Promise<any> {
+    const jwt: Payload = await this.userService.validateUser(userDTO);
+    const accessToken = this.jwtService.sign(jwt);
+    res.setHeader('Authorization', 'Bearer ' + accessToken);
+    res.cookie('AuthToken', accessToken, {
+      httpOnly: true, // xss 방지
+      maxAge: 3 * 60 * 1000, // 1000 ms
+      sameSite: 'strict', // CSRF 방지
+    });
+    return res.json({ accessToken: accessToken });
+  }
 
-    // 회원 정보 조회
-    @Get('/profile')
-    @UseGuards(UserGuard)
-    getProfile(@Req() req: Request, @Res() res: Response): any {
-        return res.send(req.user);
-    }
+  // 회원 정보 조회
+  @Get('/profile')
+  @UseGuards(UserGuard)
+  getProfile(@Req() req: Request, @Res() res: Response): any {
+    return res.send(req.user);
+  }
 
-    // 회원 정보 수정
-    @Put('/profile')
-    @UseGuards(UserGuard)
-    @UsePipes(ValidationPipe)
-    async putProfile(@Body() userDTO: UserDTO): Promise<any> {
-        return await this.userService.updateUser(userDTO);
-    }
+  // 회원 정보 수정
+  @Put('/profile')
+  @UseGuards(UserGuard)
+  @UsePipes(ValidationPipe)
+  async putProfile(@Body() userDTO: UserDTO): Promise<any> {
+    return await this.userService.updateUser(userDTO);
+  }
 
-    // 로그아웃
-    @Post('/logout')
-    logout(@Res() res: Response): any {
-        res.cookie('AuthToken', '', {
-            maxAge: 0
-        })
-        return res.send({
-            message: 'success'
-        })
-    }
+  // 로그아웃
+  @Post('/logout')
+  logout(@Res() res: Response): any {
+    res.cookie('AuthToken', '', {
+      maxAge: 0,
+    });
+    return res.send({
+      message: 'success',
+    });
+  }
 
-    // 쿠키 테스트
-    @Get('/cookie-test')
-    @UseGuards(UserGuard)
-    getCookies(@Req() req: Request, @Res() res: Response): any {
-        const jwt = req.cookies['AuthToken'];
-        return res.send(jwt);
-    }
+  // 쿠키 테스트
+  @Get('/cookie-test')
+  @UseGuards(UserGuard)
+  getCookies(@Req() req: Request, @Res() res: Response): any {
+    const jwt = req.cookies['AuthToken'];
+    return res.send(jwt);
+  }
 }

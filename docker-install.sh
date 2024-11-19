12feb3b7f13c54338b3987a880b0628fb88f782d
HS-0914 <gudtn2238@gmail.com>
2024-11-19 22:52:53 +0900

    ec2 도커 설치

--- docker-install.sh
+++ docker-install.sh
@@ -15,3 +15,7 @@ sudo add-apt-repository --yes \
 
 sudo apt-get update
 sudo apt-get install docker-ce docker-ce-cli containerd.io -y
+
+# docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
